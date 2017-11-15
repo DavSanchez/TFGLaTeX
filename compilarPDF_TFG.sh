c96d6cdf05fc8acd22aa9ec0eb3e605a5f1aa94e
DavSanchez <d.vinternatt@gmail.com>
2017-11-15 23:35:17 +0000

    commit inicial (TFG casi finalizado)

---
+++ compilarPDF_TFG.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SOURCEFILE=$1
+
+rm $SOURCEFILE.aux $SOURCEFILE.log $SOURCEFILE.bbl $SOURCEFILE.bcf $SOURCEFILE.run.xml $SOURCEFILE.blg $SOURCEFILE.toc
+pdflatex --shell-escape $SOURCEFILE
+biber $SOURCEFILE
+pdflatex --shell-escape $SOURCEFILE
+pdflatex --shell-escape $SOURCEFILE
