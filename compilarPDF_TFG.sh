c79820c9cc8ebd999fdbf3f7a8b481485f246fb5
DavSanchez <d.vinternatt@gmail.com>
2017-11-17 22:13:21 +0000

    Doble página, secciones en impar

--- compilarPDF_TFG.sh
+++ compilarPDF_TFG.sh
@@ -3,7 +3,7 @@
 SOURCEFILE=$1
 
 rm $SOURCEFILE.aux $SOURCEFILE.log $SOURCEFILE.bbl $SOURCEFILE.bcf $SOURCEFILE.run.xml $SOURCEFILE.blg $SOURCEFILE.toc
-pdflatex --shell-escape $SOURCEFILE
+pdflatex -shell-escape $SOURCEFILE
 biber $SOURCEFILE
-pdflatex --shell-escape $SOURCEFILE
-pdflatex --shell-escape $SOURCEFILE
+pdflatex -shell-escape $SOURCEFILE
+pdflatex -shell-escape $SOURCEFILE
